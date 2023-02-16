81f95770ec4b13439cd2f8769769e1d35c1bd80f
kmert0@outlook.com <kmert0@outlook.com>
2023-02-16 20:18:01 +0300

    added more route and nodemon

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res){
-    response.send("<h1>Hello</h1>");
-})
+app.get("/", function (req, res) {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: kmert0@outlook.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("My name is Mert. I'm student at AYBU.");
+});
+
+app.get("/hobbies", function (req, res) {
+    res.send("<ul><li>Basketball</li><li>Music</li></ul>");
+});
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
